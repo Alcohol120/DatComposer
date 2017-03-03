d9b461d051a463db65ddead3fdfde904045da176
Alcohol120 <alcohol120ds@gmail.com>
2017-03-04 01:46:56 +0200

    Progress

--- application/composer/Callback.py
+++ application/composer/Callback.py
@@ -11,7 +11,24 @@ class Callback:
         callback_method = getattr(self.callback, "assert_error", None)
 
         if callback_method is not None:
-            callback_method(message, title)
+            if title:
+                callback_method(message, title)
+            else:
+                callback_method(message)
+        else:
+            print(message)
+
+        pass
+
+    def assert_success(self, message, title=False):
+
+        callback_method = getattr(self.callback, "assert_success", None)
+
+        if callback_method is not None:
+            if title:
+                callback_method(message, title)
+            else:
+                callback_method(message)
         else:
             print(message)
 
